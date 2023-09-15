6ea8eaf603bfb617a8a3ae3958ce01264979bda4
RatchetDrake <bryanvilleneuve57@gmail.com>
2023-09-15 15:53:43 +0200

    Commit all

---
+++ cartepostale.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style4.css">
+    <title>Carte Postale</title>
+    </head>
+<body>
+    <div class="carte">
+        <div class="contenu">
+            <div class="adresse">
+                
+            </div>
+            <div class="message">
+           
+                <p class="texte">
+
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis quis culpa quae excepturi blanditiis at porro maiores deserunt odit harum ipsam sapiente quisquam tenetur cupiditate, distinctio libero perferendis reprehenderit sint.<br>
+                    <br>
+                    Cordialement,<br>
+                    Ton prénom
+                </p>
+            </div>
+        </div>
+        <div class="separator"></div>
+        <div class="image-carte">
+            
+            
+            <img class="tampon" src="./image/tampon.png" alt="Image 1">
+            
+            <img src="./image/timbre.png" alt="Image 3">
+            <img class="obli" src="./image/obliteration.png" alt="Image 2">
+
+            <h2>Destinataire :</h2>
+                <p>
+                    Nom du destinataire<br>
+                    Adresse<br>
+                    Ville, Pays
+                </p></div>
+    </div>
+</body>
+</html>
--- test/index.php
+++ test/index.php
@@ -3,29 +3,34 @@
 <head>
     <title>Page d'accueil</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap">
+   
 </head>
 <body>
-    <div class="container">
-        <h2>Bienvenue sur notre site !</h2>
-
-        <!-- Afficher le nom de l'utilisateur si connecté -->
-        <?php
+    <?php
         session_start();
 
         // Vérifiez si l'utilisateur est connecté (si le nom est stocké dans la session)
         if (isset($_SESSION['nom'])) {
             echo '<div class="user-info">Bonjour, ' . htmlspecialchars($_SESSION['nom']) . '</div>';
         }
-        ?>
+    ?>
 
-        <div class="links">
-            <a href="page1.php" class="button">Page1</a>
-            <a href="page2.php" class="button">Page 2</a>
-            <a href="page3.php" class="button">Page 3</a>
-        </div>
+    <!-- Lien de déconnexion -->
+    <a href="deconnexion.php" class="button">Se déconnecter</a>
 
-        <!-- Lien de déconnexion -->
-        <a href="deconnexion.php" class="button">Se déconnecter</a>
+    <div class="container">
+        <h2>Bienvenue sur notre site !</h2>
+        <!-- Contenu du conteneur -->
     </div>
+
+    <nav class="nav-links">
+        <a href="page1.php">Page 1</a>
+        <a href="page2.php">Page 2</a>
+        <a href="page3.php">Page 3</a>
+        <a href="page1.php">Page 1</a>
+        <a href="page2.php">Page 2</a>
+        <a href="page3.php">Page 3</a>
+    </nav>
 </body>
 </html>
