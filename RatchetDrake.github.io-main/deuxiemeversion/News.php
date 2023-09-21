1003a872b4391482ee33b12940243e782ea868ba
RatchetDrake <93648748+RatchetDrake@users.noreply.github.com>
2023-09-21 16:04:00 +0200

    Delete RatchetDrake.github.io-main directory

--- RatchetDrake.github.io-main/deuxiemeversion/News.php
+++
@@ -1,6 +0,0 @@
-<?php
-include('header.php');
-?>
- <link rel="stylesheet" type="text/css" href="./css/news.css">
-<title>News</title>
-
--- RatchetDrake.github.io-main/test/deconnexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Détruire la session
-session_start();
-session_destroy();
-
-// Rediriger vers la page de connexion
-header("Location: connexion.php");
-exit();
-?>
