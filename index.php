205193267c93ddf96e2ce8523b4bbab3a46a9e6e
RatchetDrake <bryanvilleneuve57@gmail.com>
2023-10-03 09:16:26 +0200

    Commit all

--- index.php
+++ index.php
@@ -1,2 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+<body>
+    <div style="text-align: center; margin-top: 100px;">
+        <h1>Bienvenue sur notre site</h1>
+        <p>Cliquez sur le bouton ci-dessous pour vous inscrire.</p>
+        <button onclick="redirectToSignUp()">S'inscrire</button>
+    </div>
 
-<?php   header("Location: index.php"); ?>
+    <script>
+        function redirectToSignUp() {
+            // Remplacez l'URL ci-dessous par l'URL de votre page d'inscription
+            window.location.href = "deuxiemeversion/inscription.php";
+        }
+    </script>
+</body>
+</html>
