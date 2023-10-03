ee175275d6bfe901ebe7cb97140a246fb62e7957
RatchetDrake <bryanvilleneuve57@gmail.com>
2023-10-03 09:24:49 +0200

    Commit all

--- index.php
+++ index.php
@@ -1,17 +1,1 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
-</head>
-<body>
-    <div style="text-align: center; margin-top: 100px;">
-        <h1>Bienvenue sur notre site</h1>
-        <p>Cliquez sur le bouton ci-dessous pour vous inscrire.</p>
-        <button onclick="redirectToSignUp()">S'inscrire</button>
-    </div>
-
-    
-</body>
-</html>
+<p>a</p>
