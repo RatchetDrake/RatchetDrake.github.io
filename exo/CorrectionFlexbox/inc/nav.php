88975285abc20ec2342539ed0f01107aa4d8bc93
RatchetDrake <bryanvilleneuve57@gmail.com>
2023-10-23 16:24:55 +0200

    Commit all

---
+++ exo/CorrectionFlexbox/inc/nav.php
@@ -0,0 +1,20 @@
+<nav>
+    <div>
+        <h1><a href=<?php
+            echo (getcwd() == '/var/www/html/cours_php/RatchetDrake.github.io/RatchetDrake.github.io/exo/CorrectionFlexbox/Vues' ? "../index.php" : "index.php")
+
+        ?>>FlexBox</a></h1>
+    </div>
+    
+    <div>
+        <a href=<?php
+            echo (getcwd() == '/var/www/html/cours_php/RatchetDrake.github.io/RatchetDrake.github.io/exo/CorrectionFlexbox/Vues' ? "audio.php" : "Vues/audio.php")
+        ?>>Audio</a>
+        <a href=<?php
+            echo (getcwd() == '/var/www/html/cours_php/RatchetDrake.github.io/RatchetDrake.github.io/exo/CorrectionFlexbox/Vues' ? "video.php" : "Vues/video.php")
+        ?>>Video</a>
+        <a href=<?php
+            echo (getcwd() == '/var/www/html/cours_php/RatchetDrake.github.io/RatchetDrake.github.io/exo/CorrectionFlexbox/Vues' ? "image.php" : "Vues/image.php")
+        ?>>Image</a>
+    </div>
+</nav>
