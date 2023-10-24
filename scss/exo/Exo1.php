9c585d52a605229dd3a58ff90978005b2b79f4d2
RatchetDrake <bryanvilleneuve57@gmail.com>
2023-10-24 16:27:24 +0200

    Commit all

---
+++ scss/exo/Exo1.php
@@ -0,0 +1,35 @@
+<?php
+// Ceci est un exemple de page PHP avec du HTML
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>My Personal Profile</title>
+    <link rel='stylesheet' type='text/css' href='Exo1.css'>
+</head>
+
+<body>
+    <div id='logo'>
+        <!-- Insérez votre logo ici -->
+    </div>
+    <h1>My Personal Profile</h1>
+    <p>Ceci est un petit texte qui ne fait pas partie des autres.</p>
+    <div class='button-container'>
+        <a href='https://www.linkedin.com' class='button'>LinkedIn</a>
+        <a href='https://www.twitter.com' class='button'>Twitter</a>
+    </div>
+    <div  class="zone">
+        <div class='text-box'>
+            <p class="p1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi nostrum dignissimos animi obcaecati beatae dolorum voluptas nihil aspernatur. Molestiae non sapiente maxime iusto minima accusantium soluta dolor, nemo sit sunt?</p>
+        </div>
+        <div class='text-box'>
+            <p class="p1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium vero sit sint delectus maxime blanditiis, voluptatibus rem officiis corporis ipsa temporibus consequatur atque illo soluta deleniti velit beatae tenetur necessitatibus.</p>
+        </div>
+        <div class='text-box'>
+            <p class="p1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro inventore ducimus, velit, atque ipsum delectus vitae voluptatem modi blanditiis deserunt voluptates. Numquam sint nulla illo distinctio qui eaque unde cumque?</p>
+        </div>
+    </div>
+</body>
+
+</html>
